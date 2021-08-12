42d3fbe76c658e6d608ce1c3f82ace531d828df2
qbaccon <qbaccon@hotmail.com>
2021-08-12 14:19:20 +0200

    update for pctr cheking if exist

--- prj_dashborad_sfny/src/Controller/PictureController.php
+++ prj_dashborad_sfny/src/Controller/PictureController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PictureController extends AbstractController
 {
     /**
-     * @Route("/", name="picture_index")
+     * @Route("/", name="picture_index", methods={"GET"})
      */
     public function index(): Response
     {
@@ -32,7 +32,7 @@ class PictureController extends AbstractController
     }
 
 	/**
-	 * @Route("/del", name="picture_del")
+	 * @Route("/del", name="picture_del", methods={"POST"})
 	 */
 	public function del_pctr(): Response
 	{
@@ -41,5 +41,28 @@ class PictureController extends AbstractController
 			\unlink('./uploads/' . $_POST['file_name']);
 			return $this->redirectToRoute('picture_index', [], Response::HTTP_SEE_OTHER);
 		}
+	}
+
+	/**
+	 * @Route("/show", name="picture_show", methods={"GET"})
+	 */
+	public function show_pctr(): Response
+	{
+		if (\file_exists('./uploads'))
+		{
+			$file = \scandir('./uploads');
+			foreach($file as $pctr)
+			{
+				if ($pctr == $_GET['name'])
+					return $this->render('picture/show_prod_pctr.html.twig', [
+						'picture' => $_GET['name'],
+						'ref' => $_GET['ref'],
+					]);
+			}
+		}
+		return $this->render('picture/show_prod_pctr.html.twig', [
+			'picture' => 'empty',
+			'ref' => $_GET['ref'],
+		]);
 	}
 }
