12cd98903d22e7735defb285e8935e9c35a20b36
qbaccon <qbaccon@hotmail.com>
2021-08-25 09:56:59 +0200

    update for disabled other button on annonce organizer chg

--- prj_annonces/script/organizer.js
+++ prj_annonces/script/organizer.js
@@ -5,12 +5,16 @@ function enbl_input(id)
 	const input_slct = document.querySelectorAll("div.annonce_box select#a" + id);
 	const btn_enbl = document.querySelector("div.annonce_box div#enbl" + id);
 	const btn_chg = document.querySelector("div.annonce_box div#chg" + id);
+	const other_btn = document.querySelectorAll("div.annonce_box div.enbl button, div.annonce_box div.del button");
 	input.forEach($elem => {
 		$elem.disabled = false;
 	});
 	input_slct.forEach($elem => {
 		$elem.disabled = false;
 	});
+	other_btn.forEach($elem => {
+		$elem.disabled = true;
+	})
 	input_ta.disabled = false;
 	btn_enbl.style.display = "none";
 	btn_chg.style.display = "flex";
