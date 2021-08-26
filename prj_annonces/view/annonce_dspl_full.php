340e10218054661758e586265cc02dada8de21f8
qbaccon <qbaccon@hotmail.com>
2021-08-26 08:38:40 +0200

    uodate style n display index

--- prj_annonces/view/annonce_dspl_full.php
+++ prj_annonces/view/annonce_dspl_full.php
@@ -12,24 +12,6 @@ foreach($annonce as $key => $value)
 	echo "<div class='annonce_details_link'>";
 	echo "<a href='view/annonce_detail.php?ann_id=$id'>Plus d'informations</a>";
 	echo "</div>";
-	// echo "<div class='details'>";
-	// echo "<div id='type'>";
-	// echo "<p class='label'>Genre</p>";
-	// echo "<p class='value'>".$value['type']."</p>";
-	// echo "</div>";
-	// echo "<div id='price'>";
-	// echo "<p class='label'>Prix (€)</p>";
-	// echo "<p class='value'>".$value['price']."</p>";
-	// echo "</div>";
-	// echo "<div id='place'>";
-	// echo "<p class='label'>Adresse</p>";
-	// echo "<p class='value'>".$value['place']."</p>";
-	// echo "</div>";
-	// echo "<div id='purpose'>";
-	// echo "<p class='label'>Description</p>";
-	// echo "<p class='value'>".$value['purpose']."</p>";
-	// echo "</div>";
-	// echo "</div>";
 	echo "</div>";
 }
 if (empty($annonce))
